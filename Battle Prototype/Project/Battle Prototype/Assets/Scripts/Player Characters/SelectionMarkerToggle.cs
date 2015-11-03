6339033ae1631b959a6eca9a06ff9e83c5a4b6f1
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-11-03 13:24:08 +0000

    Caster animation, started friendly targetting

--- Battle Prototype/Project/Battle Prototype/Assets/Scripts/Player Characters/SelectionMarkerToggle.cs
+++ Battle Prototype/Project/Battle Prototype/Assets/Scripts/Player Characters/SelectionMarkerToggle.cs
@@ -7,6 +7,8 @@ namespace Scripts.Player_Characters
     {
         private Transform _parentTransform;
         private SpriteRenderer _renderer;
+        private bool _nextSelectionSelectsFriendlyTarget;
+        private string _faction;
 
         private void OnEnable()
         {
@@ -24,11 +26,23 @@ namespace Scripts.Player_Characters
         {
             _parentTransform = transform.parent;
             _renderer = transform.GetComponent<SpriteRenderer>();
+
+            _nextSelectionSelectsFriendlyTarget = false;
+            _faction = _parentTransform.tag.Substring(0, _parentTransform.tag.IndexOf("-"));
         }
 
         private void HandleSelectionChange(Transform targetTransform)
         {
-            _renderer.enabled = (_parentTransform == targetTransform);
+            if (_nextSelectionSelectsFriendlyTarget)
+            {
+                _renderer.enabled = false;
+                _nextSelectionSelectsFriendlyTarget = false;
+            }
+            else
+            {
+                _renderer.enabled = (_parentTransform == targetTransform);
+                _nextSelectionSelectsFriendlyTarget = (targetTransform.tag == _faction + "-Healer");
+            }
         }
 
         private void HandleStatusChange(Transform source, Transform target, StatusEvent message, float value)
