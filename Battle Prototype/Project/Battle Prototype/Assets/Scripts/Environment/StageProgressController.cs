1ecc29b6789538733d196d8cf832242c70324f3b
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-11-23 21:31:44 +0000

    Started adding environment

---
+++ Battle Prototype/Project/Battle Prototype/Assets/Scripts/Environment/StageProgressController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Scripts.Event_Dispatchers;
+
+namespace Scripts.Environment
+{
+    public class StageProgressController : MonoBehaviour
+    {
+        private Transform _transform;
+        private int _movementDirection;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        private void OnEnable()
+        {
+            UIEventDispatcher.ButtonEventHandler += HandleButtonEvent;
+        }
+
+        private void OnDisable()
+        {
+            UIEventDispatcher.ButtonEventHandler -= HandleButtonEvent;
+        }
+
+        private void HandleButtonEvent(string buttonName, bool isPressed)
+        {
+            _movementDirection = 0;
+        }
+    }
+}
