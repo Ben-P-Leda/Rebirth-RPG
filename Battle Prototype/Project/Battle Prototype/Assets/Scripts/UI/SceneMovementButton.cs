a72f718ecef8fab41843f0c99b67ee546976df2d
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-11-23 13:25:15 +0000

    Movement buttons on screen,

---
+++ Battle Prototype/Project/Battle Prototype/Assets/Scripts/UI/SceneMovementButton.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using Scripts.Event_Dispatchers;
+using Scripts.UI;
+
+namespace Scripts.UI
+{
+    public class SceneMovementButton : MonoBehaviour
+    {
+        private Rect _displayArea;
+        private bool _held;
+        private string _buttonName;
+
+        public int MovementStep;
+        public Texture2D Texture;
+
+        public UIEventDispatcher UIEventDispatcher { private get; set; }
+
+        public SceneMovementButton()
+        {
+            _held = false;
+            _buttonName = "";
+
+            UIEventDispatcher = null;
+        }
+
+        private void Awake()
+        {
+            _buttonName = transform.name;
+        }
+
+        private void Start()
+        {
+            float offset = (Screen.width - (Texture.width * UIUtilities.Scaling)) * 0.5f;
+
+            _displayArea = new Rect(
+                offset + (offset * MovementStep),
+                Screen.height - (Texture.height * UIUtilities.Scaling),
+                Texture.width * UIUtilities.Scaling,
+                Texture.height * UIUtilities.Scaling);
+        }
+
+        private void OnGUI()
+        {
+            bool wasHeld = _held;
+            _held = GUI.RepeatButton(_displayArea, Texture);
+
+            if (wasHeld != _held)
+            {
+                Debug.Log(string.Format("Button {0} held state updated to {1}", _buttonName, _held));
+                if (UIEventDispatcher != null)
+                {
+                    UIEventDispatcher.FireUIButtonEvent(_buttonName, _held);
+                }
+            }
+        }
+    }
+}
---
+++ Battle Prototype/Project/Battle Prototype/Assets/Scripts/UI/UIUtilities.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Scripts.UI
+{
+    public static class UIUtilities
+    {
+        private static float _scaling = 0.0f;
+        public static float Scaling
+        {
+            get
+            {
+                if (_scaling == 0.0f)
+                {
+                    _scaling = Screen.height / One_To_One_Screen_Height;
+                }
+                return _scaling;
+            }
+        }
+
+        private const float One_To_One_Screen_Height = 675.0f;
+    }
+}
